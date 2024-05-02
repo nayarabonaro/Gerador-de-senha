3e0c89c4b6c06a1fd414c0a2d92b7481a64eb4cb
nayarabonaro <167829390+nayarabonaro@users.noreply.github.com>
2024-05-02 20:40:48 -0300

    Atualizar o script.js

--- script.js
+++ script.js
@@ -1,26 +1,26 @@
 
-let sliderElement = document. querySelector("#slider");
-let buttonElement = document. querySelector("#button");
-let sizePassword = document.querySelector("#valor");
+let sliderElement = document.querySelector("#slider"');
+let buttonElement = document.querySelector("#button");
+let sizePassword = document.querySelector( #valor");
 let password = document.querySelector("#password");
-let containerPassword = document.querySelector("#container-password");
-let charset = 'abcdefghijk|mnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ01234567891';
-let novaSenha ="
-sizePassword.innerHTML = sliderElement.value;
-sider.oninput = tunction ) {
-sizePassword.innerHTML = this.value;
+let containerl assword = document.querySelector("#container-password");
+let charset = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!';
+let novaSenha =";
+sizePassword.innerHTML=sliderElement.value,
+slider.oninput = function ) {
+ sizePassword.innerHTML = this.value;
 }
-function generate assword(
-let pass =
-for(let i = 0, n = charset. length; i < sliderElement. value; ++|).
-pass += charset.charAt(Math. floor(Math.random() * n));
+function generatePassword(){
+let pass =";
+for(let i = 0, n = charset.length; i < sliderElement.value; ++i){
+pass += charset.charAt(Math.floor(Math.random() * n));
+}
 console.log(pass)
 containerPassword.classList.remove("hide");
 password.innerHTML = pass;
-novaSenha = pass;
+novasenha = pass;
 }
 function copyPassword(){
 alert("Senha copiada com sucesso!")
-navigator.clipboard. write Text(novaSenha);
+navigator.clipboard.writeText(novaSenha);
 }
-
